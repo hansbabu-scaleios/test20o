7094d347222ae04cd1ebb3055187d256c6aa5bd5
abijith2233 <abijith.mv001@gmail.com>
2023-01-25 16:39:43 +0530

    Added User Model and its DTOs, AB#7581

---
+++ AI.Finder.BE.Service/Features/User/UserModelConfiguration.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AI.Finder.BE.Service.Features.User;
+
+public class UserModelConfiguration : IEntityTypeConfiguration<UserModel>
+{
+    public void Configure(EntityTypeBuilder<UserModel> builder)
+    {
+        builder.ToTable("user");
+        builder.HasKey(e => e.Id);
+        builder.HasKey(m => m.UserId);
+        builder.Property(e => e.Id).ValueGeneratedOnAdd();
+        builder.Property(m => m.UserId).ValueGeneratedOnAdd();
+        builder.HasIndex(m => m.UserId).IsUnique();
+        builder.Property(e => e.EmailTokenGeneratedTimestamp)
+            .HasColumnType("timestamp without time zone");
+        builder.Property(e => e.PhoneTokenGeneratedTimestamp)
+            .HasColumnType("timestamp without time zone");
+        //TODO: CandidateId & RelationId to be added after creation of candidate and addresstype models
+    }
+}
---
+++ AI.Finder.BE.Service/Features/User/UserResponseDTO.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AI.Finder.BE.Service.Features.User
+{
+    public class UserResponseDTO
+    {
+        
+    }
+}
