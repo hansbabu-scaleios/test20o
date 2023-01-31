5797a00d066fe7bfe1abce34b40fde43279dd44e
abijith2233 <abijith.mv001@gmail.com>
2023-01-31 13:25:25 +0530

    Updated nswag, AB#7661

--- AI.Finder.BE.Service/Program.cs
+++ AI.Finder.BE.Service/Program.cs
@@ -14,7 +14,6 @@ builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerDocument(configure => configure.Title = "Finder API V1");
-//builder.Services.AddSwaggerGen();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 var app = builder.Build();
 
@@ -23,16 +22,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseMiddleware<ExceptionHandlingMiddleware>();
     app.UseOpenApi();
-    /*
-    //app.UseSwaggerUI();
-    app.UseSwaggerUI(c =>
-   {
-       c.SwaggerEndpoint("/swagger/v1/swagger.json", "Finder API V1");
-       c.RoutePrefix = string.Empty;
-       c.DefaultModelsExpandDepth(-1);
-   });
-   */
-   app.UseSwaggerUi3();
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
