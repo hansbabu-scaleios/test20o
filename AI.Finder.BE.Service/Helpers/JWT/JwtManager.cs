07cae4a18c70e2a4414c85a76378c172376bf9dd
AkshayNr677 <99013615+AkshayNr677@users.noreply.github.com>
2023-02-01 14:07:42 +0530

    jwt config change

--- AI.Finder.BE.Service/Helpers/JWT/JwtManager.cs
+++ AI.Finder.BE.Service/Helpers/JWT/JwtManager.cs
@@ -1,15 +1,12 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
-using AI.Finder.BE.Service.Features.User;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
 namespace AI.Finder.BE.Service.Helpers.JWT;
 public class JwtManager
 {
-    public static  JwtSecurityToken GenerateJwtToken(string UserId,string ExpiryDate)
+    public static JwtSecurityToken GenerateJwtToken(string UserId,string ExpiryDate)
     {
         var claims = new[]{
                        new Claim(JwtClaimConstants.UserId, UserId),
@@ -18,14 +15,14 @@ public class JwtManager
                     };
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("key")));
         var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-        var token = new  JwtSecurityToken(
+        var token = new  JwtSecurityToken (
                        Environment.GetEnvironmentVariable("Issuer"),
                        Environment.GetEnvironmentVariable("Audience"),
                        claims,
                        expires: DateTime.Now.AddMinutes(Convert.ToInt32(ExpiryDate)),
                        signingCredentials: credential
                     );
-        return  token;
+        return   token;
     }
     public  static   JwtClaimsValue DecodeGeneratedToKen(HttpContext context)
     {
