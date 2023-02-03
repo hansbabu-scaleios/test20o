b4be8b8b70e007120632547346acc57208e5c20f
abijith2233 <abijith.mv001@gmail.com>
2023-02-03 16:09:53 +0530

    updated

---
+++ AI.Finder.BE.Service/Features/ActivityType/ActivityTypeModel.cs
@@ -0,0 +1,6 @@
+namespace AI.Finder.BE.Service.Features.ActivityType;
+    public class ActivityTypeModel{
+        public long Id { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; }
+    }
